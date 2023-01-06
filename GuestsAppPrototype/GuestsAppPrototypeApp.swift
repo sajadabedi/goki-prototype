8855c49ff74547cff791a917ceaf7aeb738a3bfe
Sajjad <sajad.abedi@veriff.net>
2023-01-06 20:04:18 +0200

    Initial Commit

---
+++ GuestsAppPrototype/GuestsAppPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestsAppPrototypeApp.swift
+//  GuestsAppPrototype
+//
+//  Created by Sajad Abedi on 06.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuestsAppPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
