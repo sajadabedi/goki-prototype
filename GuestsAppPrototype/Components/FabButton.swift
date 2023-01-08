5644bc59ea025e9cf07df7791abb7800c5b65252
Sajjad <sajad.abedi@veriff.net>
2023-01-08 13:06:39 +0200

    Fab button and Concierge

---
+++ GuestsAppPrototype/Components/FabButton.swift
@@ -0,0 +1,32 @@
+//
+//  FabButton.swift
+//  GuestsAppPrototype
+//
+//  Created by Sajad Abedi on 08.01.2023.
+//
+
+import SwiftUI
+
+struct FabButton: View {
+    var body: some View {
+        Button {
+            //action
+        } label: {
+            Circle()
+                .frame(width: 60, height: 60)
+                .shadow(color: .black.opacity(0.2), radius: 8,y: 8)
+                .overlay {
+                    Image(systemName: "hands.sparkles")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                }
+        }
+    }
+}
+
+struct FabButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FabButton()
+    }
+}
--- GuestsAppPrototype/ContentView.swift
+++ GuestsAppPrototype/ContentView.swift
@@ -11,20 +11,28 @@ import Shimmer
 struct ContentView: View {
     let facilities = ["Wifi","Washer","TV","Bathtub","Bike", "PlayStation"]
     let smartKeysArray = [["Room","bed.double.fill"],["Main door", "bed.double.fill"]]
+    @State var firstOpen: Bool = false
     var body: some View {
         NavigationStack{
-            ScrollView{
-                VStack(alignment: .trailing) {
-                    NavigationBar
-                    VStack(spacing: 18){
-                        ReservationCard
-                        Divider()
-                        SamrtKeys.padding(.bottom)
-                        Divider()
+            ZStack(alignment:.bottomTrailing){
+                ScrollView (showsIndicators: false){
+                    ZStack{
+                        VStack(alignment: .trailing) {
+                            NavigationBar
+                            VStack(spacing: 18){
+                                ReservationCard
+                                Divider()
+                                SamrtKeys
+                                Divider()
+                                ConciergeList
+                            }
+                            Spacer()
+                        }
+                        
                     }
-                    Spacer()
+                    .padding()
                 }
-                .padding()
+                FabButton().padding(.horizontal)
             }
         }
     }
@@ -60,8 +68,7 @@ extension ContentView {
             Image("hotelCover")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: .infinity, height: 170)
-                
+                .frame(height: 170)
                 .SmoothCorner(radius: 8)
                 .shadow(color: .gray.opacity(0.2), radius: 5, y:5)
                 .padding(.bottom)
@@ -94,39 +101,99 @@ extension ContentView {
     // MARK: Smart Keys
     private var SamrtKeys: some View {
         VStack(alignment:.leading){
-            Text("Open Door")
-                .font(.title3)
-                .bold()
-                .shimmering()
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Open Door")
+                        .font(.title3)
+                        .bold()
+                        .shimmering(active: firstOpen ? false : true,duration: 2.5)
+                        
+                    Text("Tap and hold to open")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+                Button {
+                    
+                } label: {
+                    HStack{
+                        Image(systemName: "entry.lever.keypad")
+                        Text("Door Code")
+                            .font(.subheadline)
+                            .bold()
+                    }
+                }
+                .buttonStyle(.borderless)
+            }.padding(.bottom, 4)
+                
             ScrollView(.horizontal, showsIndicators: false){
                 HStack{
-                    SmartKeyButtonView(iconName: "bed.double.fill", label: "Room")
+                    SmartKeyButtonView(iconName: "bed.double.fill", label: "Room").onTapGesture {
+                        firstOpen = true
+                        print(firstOpen)
+                    }
                     SmartKeyButtonView(iconName: "door.left.hand.closed", label: "Main Entrance")
                     SmartKeyButtonView(iconName: "bicycle", label: "Bike Shed")
                     SmartKeyButtonView(iconName: "bicycle", label: "Bike Shed")
                 }
             }.padding(.bottom)
-            
-            Button {
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var ConciergeList: some View {
+        VStack(alignment:.leading){
+            Text("Concierge")
+                .font(.title3)
+                .bold()
+                .padding(.bottom)
+            VStack(spacing: 12){
+                HStack(alignment:.top){
+                    Image(systemName: "calendar.badge.plus").frame(width: 32)
+                    VStack(alignment: .leading){
+                        Text("Extend stay")
+                            .bold()
+                        Text("Request to stay longer")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
                 
-            } label: {
-                Image(systemName: "entry.lever.keypad")
-                Text("Door Code")
-                    .font(.subheadline)
-                    .bold()
-                    
-                    
-            }
-            .foregroundColor(.primary)
-            .padding(12)
-            .background(.regularMaterial)
-            .SmoothCorner(radius: 8)
-            .frame(maxWidth: .infinity)
-            
-            
-            
-
-            
+                HStack(alignment:.top){
+                    Image(systemName: "wifi").frame(width: 32)
+                    VStack(alignment: .leading){
+                        Text("Wi-fi")
+                            .bold()
+                        Text("Join the guest Wi-Fi")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack(alignment:.top){
+                    Image(systemName: "person.2.fill").frame(width: 32)
+                    VStack(alignment: .leading){
+                        Text("Share access")
+                            .bold()
+                        Text("Share your keys with your family")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack(alignment:.top){
+                    Image(systemName: "bicycle").frame(width: 32)
+                    VStack(alignment: .leading){
+                        Text("Rent a Bike")
+                            .bold()
+                        Text("Under 5min with great price")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+            }.frame(maxWidth: .infinity, alignment: .leading)
+               
         }.frame(maxWidth: .infinity, alignment: .leading)
     }
 }
